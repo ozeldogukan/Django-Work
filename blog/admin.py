218d6f0617262723ba83384d56b7254ef20470c2
ozeldogukan <ozeldogukan@ebrandvalue.com>
2022-09-13 13:16:27 +0300

    PEP-8 Changes

--- blog/admin.py
+++ blog/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from .models import Blog, Category
 from django.utils.safestring import mark_safe
 
+
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("title","is_active","is_home", "slug","selected_categories",)
-    list_editable = ("is_active","is_home",)
-    search_fields = ("title","description",)
+    list_display = ("title", "is_active", "is_home", "slug", "selected_categories",)
+    list_editable = ("is_active", "is_home",)
+    search_fields = ("title", "description",)
     readonly_fields = ("slug",)
-    list_filter = ("is_active","is_home","categories",)
+    list_filter = ("is_active", "is_home", "categories",)
 
     def selected_categories(self, obj):
         html = "<ul>"
@@ -15,6 +16,7 @@ class BlogAdmin(admin.ModelAdmin):
             html += "<li>" + category.name + "</li>"
         html += "</ul>"
         return mark_safe(html)
+
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Category)
